a4b2d51b6b5cffe26f5851bbe4f5bb89cc5a6657
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-04-30 01:32:36 +0500

    get company images and start to set now

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/header.jsx";
 import Hero from "./components/hero.jsx";
+import Organizatons from "./components/organizatons.jsx";
 function App() {
   return (
     <>
       <div className="w-full h-screen ">
         <Header />
         <Hero />
+        <Organizatons/>
       </div>
     </>
   );
---
+++ src/components/organizatons.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+
+const Organizatons = () => {
+  return (
+    <div>
+      <div class="mt-12 grid grid-cols-3 sm:grid-cols-4 md:grid-cols-6">
+        <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+          <img
+            src="/public/images/clients/microsoft.svg"
+            class="h-12 w-auto mx-auto"
+            loading="lazy"
+            alt="client logo"
+            width=""
+            height=""
+          />
+        </div>
+        <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+          <img
+            src="/public/images/clients/airbnb.svg"
+            class="h-12 w-auto mx-auto"
+            loading="lazy"
+            alt="client logo"
+            width=""
+            height=""
+          />
+        </div>
+        <div class="p-4 flex grayscale transition duration-200 hover:grayscale-0">
+          <img
+            src="/public/images/clients/google.svg"
+            class="h-9 w-auto m-auto"
+            loading="lazy"
+            alt="client logo"
+            width=""
+            height=""
+          />
+        </div>
+        <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+          <img
+            src="/public/images/clients/ge.svg"
+            class="h-12 w-auto mx-auto"
+            loading="lazy"
+            alt="client logo"
+            width=""
+            height=""
+          />
+        </div>
+        <div class="p-4 flex grayscale transition duration-200 hover:grayscale-0">
+          <img
+            src="/public/images/clients/netflix.svg"
+            class="h-8 w-auto m-auto"
+            loading="lazy"
+            alt="client logo"
+            width=""
+            height=""
+          />
+        </div>
+        <div class="p-4 grayscale transition duration-200 hover:grayscale-0">
+          <img
+            src="./../../public/images/clients/google-cloud.svg"
+            class="h-12 w-auto mx-auto"
+            loading="lazy"
+            alt="client logo"
+            width=""
+            height=""
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Organizatons;
