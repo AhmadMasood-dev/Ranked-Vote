2ce62c918d624b9bf010fbca77ebf4970bffddb4
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-05-02 22:47:22 +0500

    cta section

--- src/components/cta.jsx
+++ src/components/cta.jsx
@@ -7,10 +7,10 @@ function Cta(props) {
         <div class="container md:px-10 px-5 py-24 mx-auto flex items-center md:flex-row flex-col">
           <div class="flex flex-col md:pr-10 md:mb-0 mb-6 pr-0 w-full md:w-auto md:text-left text-center">
             <h2 class="text-xs text-lavender tracking-widest font-medium title-font mb-1">
-            GET STARTED IN MINUTES...
+              GET STARTED IN MINUTES...
             </h2>
             <h1 class="md:text-4xl text-2xl font-bold title-font text-white">
-            Poll your voters now
+              Poll your voters now
             </h1>
           </div>
           <div class="flex md:ml-auto md:mr-0 mx-auto items-center flex-shrink-0 space-x-4">
@@ -28,10 +28,9 @@ function Cta(props) {
                 <span class="title-font font-medium">Google Play</span>
               </span>
             </button> */}
-             <button class="inline-flex text-white bg-black border-0 py-2 px-6 focus:outline-none hover:bg-midnight-blue transition duration-700 ease-out rounded-full text-lg">
+            <button class="inline-flex text-white bg-black border-0 py-2 px-6 focus:outline-none hover:bg-midnight-blue transition duration-700 ease-out rounded-full text-lg">
               Get Started
             </button>
-            
           </div>
         </div>
       </section>
