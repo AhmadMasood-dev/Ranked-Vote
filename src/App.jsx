5bbccb802f71740cce7afb0926b0dbcb9fb4b089
Ahmad Masood <ahmadmasood.dev@gmail.com>
2024-05-03 00:26:03 +0500

    make footer section

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import FeatureSection from "./components/featureSection.jsx";
 import Header from "./components/header.jsx";
 import Hero from "./components/hero.jsx";
 import Organizatons from "./components/organizatons.jsx";
-import Part from './components/organizatonsPart2.jsx'
-import PictureData from './components/pictureData.jsx'
-import VotersRank from './components/votersRank.jsx'
+import Part from "./components/organizatonsPart2.jsx";
+import PictureData from "./components/pictureData.jsx";
+import VotersRank from "./components/votersRank.jsx";
 import Cta from "./components/cta.jsx";
+import Footer from "./components/footer.jsx";
 // import Slider from './components/slider.jsx'
 import Statistic from "./components/statistic.jsx";
 function App() {
@@ -15,13 +16,14 @@ function App() {
       <div className="w-full h-screen ">
         <Header />
         <Hero />
-        <Organizatons/>
-        <Part/>
-        <FeatureSection/>
-        <PictureData/>
-        <VotersRank/>
-       <Statistic/>
-       <Cta/>
+        <Organizatons />
+        <Part />
+        <FeatureSection />
+        <PictureData />
+        <VotersRank />
+        <Statistic />
+        <Cta />
+        <Footer />
       </div>
     </>
   );
